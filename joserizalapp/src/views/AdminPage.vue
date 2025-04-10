9ae34269894788235e03314b0412741b64f0be96
LynnDelaere <lynn.delaere@hotmail.be>
2025-04-10 13:40:22 +0200

    Made an add city card

--- joserizalapp/src/views/AdminPage.vue
+++ joserizalapp/src/views/AdminPage.vue
@@ -1,20 +1,21 @@
 <template>
     <div id="adminPage" class="pageContent">
         <h1>Admin Page</h1>
-        <div class="adminPageContent">
-            <!-- <AddEditPageComponent /> -->
-            <h1> Add location </h1>
+        <div class="addContent">
             <AddLocationComponent />
-            <h1> Add article </h1>
             <AddArticleComponent />
+        </div>
+        <div class="displayContent">
+            <h2>City List</h2>
+            <CityListComponent />
         </div>
     </div>
 </template>
 
 <script setup>
-import AddEditPageComponent from '@/components/EditPageComponent.vue'
 import AddLocationComponent from '@/components/AddLocationComponent.vue'
 import AddArticleComponent from '@/components/AddArticleComponent.vue'
+import CityListComponent from '@/components/CityListComponent.vue'
 </script>
 
 <script>
@@ -24,5 +25,26 @@ export default {
 </script>
 
 <style>
+.pageContent {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+}
+.addContent {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    margin-bottom: 20px;
+}
 
+.addContent>* {
+    flex: 1;
+}
+
+@media (max-width: 768px) {
+    .addContent {
+        flex-direction: column;
+    }
+}
 </style>
