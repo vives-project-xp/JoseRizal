07464be461fb3367fd43dafa4c063b2c39b00e6e
Alexander D'hoore <alexander.dhoore@vives.be>
2025-04-21 15:25:34 +0200

    Translate chinese to english

--- database/db_config.py
+++ database/db_config.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-# 从环境变量获取数据库配置
+# Load database configuration from environment variables
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-# 创建数据库引擎
+
+# Create the database engine
 engine = create_engine(DATABASE_URL)
 
-# 创建 Session
+# Create a Session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
