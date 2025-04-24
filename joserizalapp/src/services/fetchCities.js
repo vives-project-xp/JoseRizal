ff9851977635876fa85a5cd0cd8f9b8eede4ddeb
tscshawn <tristansh4wn@gmail.com>
2025-04-24 12:28:21 +0200

    Initial city & custom tour page, Placeholder pictures for other stuff

--- joserizalapp/src/services/fetchCities.js
+++ joserizalapp/src/services/fetchCities.js
@@ -1,24 +1,28 @@
 import generic_city from '../assets/generic_city.jpg'
 
+import img_ghent from '../assets/generic_city_pics/Ghent.jpg'
+import img_madrid from '../assets/generic_city_pics/Madrid.jpg'
+import img_amsterdam from '../assets/generic_city_pics/Amsterdam.jpg'
+
 const fetchCities = () => {
     return [
         {
             id: 1,
-            title: 'Gent',
+            title: 'Ghent',
             content: '# This is some content',
-            imageUrl: generic_city,
+            imageUrl: img_ghent,
         },
         {
             id: 2,
             title: 'Madrid',
             content: '# This is some content',
-            imageUrl: generic_city,
+            imageUrl: img_madrid,
         },
         {
             id: 3,
             title: 'Amsterdam',
             content: '# This is some content',
-            imageUrl: generic_city,
+            imageUrl: img_amsterdam,
 
         }
     ]
--- joserizalapp/src/views/CityPage.vue
+++ joserizalapp/src/views/CityPage.vue
@@ -1,26 +1,69 @@
 <template>
 	<div class="page-container">
-		<div class="city-page">
+        <div v-if="isLoading">Loading article...</div>
+        <div v-else-if="error">{{ error }}</div>
+		<div v-else class="city-page">
         	<!-- TOURS -->
-        <h1>City</h1>
+        <h1>{{ selectedCity.title }}</h1>
+        <p>Explore and discover all of {{selectedCity.title }}'s landmarks related to Jose Rizal...</p>
         <FeaturedTour 
-            :imageUrl='cityImage'
-            title="City"
-            :cityId="1238"
+            :imageUrl='selectedCity.imageUrl'
+            :title='selectedCity.title'
+            :cityId='selectedCity.id'
         />
         
-		<h3>Customise Your tour</h3>
-		
+        <PageBreakComponent />
+		<h3>Customise Your Tour</h3>
+        <p>Alternatively, you can customise your tour of {{ selectedCity.title }} instead...</p>
         <CustomTour />
     	</div>
 	</div>
 </template>
 
 <script setup>
-
 import FeaturedTour from '../components/UserFrontend/FeaturedTour.vue';
 import CustomTour from '../components/UserFrontend/CustomTour.vue';
+
+import PageBreakComponent from '@/components/UserFrontend/PageBreakComponent.vue';
+
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import fetchCities from '@/services/fetchCities';
+
+const route = useRoute()
+const listOfCities = ref([])
+const selectedCity = ref({})
+
+const isLoading = ref(true)
+const error = ref(null)
+
 import cityImage  from '../assets/generic_city.jpg';
+
+const fetchCity = async(id) => {
+    await new Promise(resolve => setTimeout(resolve, 500))
+
+    console.log("THIS IS THE CITY ID CHOSEN: ", id)
+
+    listOfCities.value = fetchCities()
+
+    return listOfCities.value[id-1] || null
+}
+
+onMounted(async () => {
+    try {
+        const result = await fetchCity(route.params.id)
+        if (result) {
+        selectedCity.value = result
+        } else {
+        error.value = 'City not found'
+        }
+    } catch (err) {
+        console.log(err)
+        error.value = 'Failed to load selected city'
+    } finally {
+        isLoading.value = false
+    }
+})
 
 </script>
 
@@ -32,5 +75,9 @@ import cityImage  from '../assets/generic_city.jpg';
     align-items: center;
     text-align: center;
     position: relative;
+}
+
+p {
+    margin-bottom: 2rem;
 }
 </style>
